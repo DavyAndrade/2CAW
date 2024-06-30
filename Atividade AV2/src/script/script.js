74bf2541876a7cd6194838d3d5db491df1192506
DavyAndrade <hallszindementahhh@gmail.com>
2024-06-29 22:13:14 -0300

    Construindo Catálogo

--- Atividade AV2/src/script/script.js
+++ Atividade AV2/src/script/script.js
@@ -3,32 +3,48 @@
 let currentSlide = 0;
 
 function showSlide(index) {
-    const slides = document.querySelectorAll('.carrossel-item');
-    if (index >= slides.length) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = index;
-    }
+  const slides = document.querySelectorAll(".carrossel-item");
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
 
-    const offset = -currentSlide * 100;
-    document.querySelector('.carrossel-inner').style.transform = `translateX(${offset}%)`;
+  const offset = -currentSlide * 100;
+  document.querySelector(
+    ".carrossel-inner"
+  ).style.transform = `translateX(${offset}%)`;
 
-    slides.forEach((slide, idx) => {
-        slide.classList.toggle('active', idx === currentSlide);
-    });
+  slides.forEach((slide, idx) => {
+    slide.classList.toggle("active", idx === currentSlide);
+  });
 }
 
 function moveSlide(direction) {
-    showSlide(currentSlide + direction);
+  showSlide(currentSlide + direction);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentSlide);
 });
 
 // Troca de Temas
-document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+  // Salva o tema atual no localStorage
+  const isDarkTheme = document.body.classList.contains("dark-theme");
+  localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+}
+
+// Evento de troca de tema
+document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+
+// Carrega o tema salvo ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+  }
 });
